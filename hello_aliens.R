eee5030d0697f73065414991de7f0ec45f017ce5
ybing97 <33290138+ybing97@users.noreply.github.com>
2017-12-10 20:20:20 -0500

    Update hello_aliens.R

--- hello_aliens.R
+++ hello_aliens.R
@@ -1,6 +1,6 @@
 cat("Hi Aliens! I am from the planet Earth, and have been selected as a spokesperson for all humans. 
 I am currently taking a course called Statistical Computing, where I get to learn R.
-But you must be wondering, "What is life on Earth like?" Life on Earth is wonderful! How is life as an Alien?
+But you must be wondering, 'What is life on Earth like?' Life on Earth is wonderful! How is life as an Alien?
 What do you look like? Do you have plans to visit Earth someday? 
 If so, I hope we can meet one day and become friends!
 Have a great day!")
